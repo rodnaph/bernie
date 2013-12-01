4bf538e2eef07a99fed1824116a51679406fb3e9
rod <rod@pu-gh.com>
2013-12-01 21:34:12 +0000

    fixed project def

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 
-(defproject rodnaph/bernie "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject rodnaph/bernie "0.1.0"
+  :description "Unserialize PHP data in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"])
