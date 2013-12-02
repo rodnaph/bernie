3a7a8fd2159021b9745d19dbaa98c41a03f75a2c
Rhodri Pugh <rod@pu-gh.com>
2013-12-02 11:15:13 +0000

    bump to 0.1.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject rodnaph/bernie "0.1.1"
+(defproject rodnaph/bernie "0.1.2"
   :description "Unserialize PHP data in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clojure"]
